f8a0544c10a069d78807e52b150de1ce2528b844
ManuelGF <manuelg139@gmail.com>
2021-04-15 02:28:40 -0500

    Got Dashbaord and Craete to work

--- seeds/projectData.js
+++ seeds/projectData.js
@@ -4,28 +4,28 @@ const projectdata = [
     {
         title: 'ReadMe Generator',
         media_link: 'https://youtu.be/LJDEDQDcHeM',
-        description: 'How to install and use readme generator'
+        description: 'How to install and use readme generator',
+        users_id: '1',
     },
     {
         title: 'Team Profile Generator',
         media_link: 'https://youtu.be/GQsDzlJ7brE',
-        description: 'How to install and use profile generator'
+        description: 'How to install and use profile generator',
+        users_id: '2',
     },
     {
         title: 'Team Profile Generator',
         media_link: 'https://youtu.be/GQsDzlJ7brE',
-        description: 'How to install and use profile generator'
+        description: 'How to install and use profile generator',
+        users_id: '3',
     },
     {
         title: 'Team Profile Generator',
         media_link: 'https://youtu.be/GQsDzlJ7brE',
-        description: 'How to install and use profile generator'
+        description: 'How to install and use profile generator',
+        users_id: '2',
     },
-    {
-        title: 'Team Profile Generator',
-        media_link: 'https://youtu.be/GQsDzlJ7brE',
-        description: 'How to install and use profile generator'
-    },
+    
 ];
 
 const seedProject = () => Projects.bulkCreate(projectdata);
